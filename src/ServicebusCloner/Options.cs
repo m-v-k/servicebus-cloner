e90c6c4c6d5625327448dffae6bbb6fd9949c903
michiel.van.klinken <michiel.van.klinken@syncforce.com>
2023-08-31 12:49:15 +0200

    environment variables support

--- src/ServicebusCloner/Options.cs
+++ src/ServicebusCloner/Options.cs
@@ -2,43 +2,43 @@ using CommandLine;
 
 public class Options
 {
-    [Option('t', "tenant", Required = true, HelpText = "The tenant id.")]
+    [Option('t', "tenant", Required = true, HelpText = "The tenant id. [SC_TENANT]")]
     public string TenantId { get; set; }
 
-    [Option('s', "subscription", Required = true, HelpText = "The subscription id.")]
+    [Option('s', "subscription", Required = true, HelpText = "The subscription id. [SC_SUBSCRIPTION]")]
     public string SubscriptionId { get; set; }
 
-    [Option("clientid", Required = true, HelpText = "The clientid.")]
+    [Option("clientid", Required = true, HelpText = "The clientid. [SC_CLIENTID]")]
     public string ClientId { get; set; }
     
-    [Option("clientsecret", Required = true, HelpText = "The clientsecret.")]
+    [Option("clientsecret", Required = true, HelpText = "The clientsecret. [SC_CLIENTSECRET]")]
     public string ClientSecret { get; set; }
 
-    [Option("sbFrom", Required = true, HelpText = "Service Bus Name from which to clone.")]
+    [Option("sbFrom", Required = true, HelpText = "Service Bus Name from which to clone. [SC_FROM]")]
     public string ServiceBusFrom { get; set; }
     
-    [Option("sbTo", Required = true, HelpText = "Service Bus Name for the temporary created one.")]
+    [Option("sbTo", Required = true, HelpText = "Service Bus Name for the temporary created one. [SC_TO]")]
     public string ServiceBusTo { get; set; }
 
-    [Option("region", Required = false, HelpText = "The azure region for the new namespace.", Default = "westeurope")]
+    [Option("region", Required = false, HelpText = "The azure region for the new namespace. [SC_REGION]", Default = "westeurope")]
     public string AzureRegion { get; set; }
 
-    [Option("resourcegroup", Required = false, HelpText = "The resource group where temporary namespace is created.", Default = "servicebuscloner")]
+    [Option("resourcegroup", Required = false, HelpText = "The resource group where temporary namespace is created. [SC_RESOURCEGROUP]", Default = "servicebuscloner")]
     public string ResourceGroup { get; set; }
 
-    [Option("sku", Required = false, HelpText = "The namespace sku.", Default = "Standard")]
+    [Option("sku", Required = false, HelpText = "The namespace sku. [SC_SKU]", Default = "Standard")]
     public string Sku { get; set; }
 
-    [Option("create", SetName = "create", Required = false, HelpText = "Create a service bus and clone from an existing one. The create and delete options are mutually exclusive.", Default = true)]
+    [Option("create", SetName = "create", Required = false, HelpText = "Create a service bus and clone from an existing one. The create and delete options are mutually exclusive. [SC_CREATE]", Default = true)]
     public bool Create { get; set; }
     
-    [Option("delete", SetName = "delete", Required = false, HelpText = "Delete a service bus. The create and delete options are mutually exclusive.")]
+    [Option("delete", SetName = "delete", Required = false, HelpText = "Delete a service bus. The create and delete options are mutually exclusive. [SC_DELETE]")]
     public bool Delete { get; set; }
     
-    [Option('o',"out", Required = false, HelpText = "File path where the temporary servicebus connectionstring is saved to.", Default = "/app/out/connectionstring")]
+    [Option('o',"out", Required = false, HelpText = "File path where the temporary servicebus connectionstring is saved to. [SC_OUT]", Default = "/app/out/connectionstring")]
     public string OutFile { get; set; }
     
-    [Option("ephemeral", SetName = "ephemeral", Required = false, HelpText = "Keep service bus running while container is running. Deletes the resources when container stops.")]
+    [Option("ephemeral", SetName = "ephemeral", Required = false, HelpText = "Keep service bus running while container is running. Deletes the resources when container stops. [SC_EPHEMERAL]")]
     public bool Ephemeral { get; set; }
     
     // Add other properties as needed...
--- src/ServicebusCloner/Program.cs
+++ src/ServicebusCloner/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-
+﻿using System.Reflection;
 using CommandLine;
 
 namespace ServiceBusCloner.Service;
@@ -11,6 +9,8 @@ public class Program
 
     static void Main(string[] args)
     {
+        args = AppendEnvironmentVariables(args);
+
         var isEphemeral = args.Contains("--ephemeral");
         if (isEphemeral)
         {
@@ -35,7 +35,7 @@ public class Program
             };
         }
 
-        Parser.Default.ParseArguments<Options>(args)
+        var result = Parser.Default.ParseArguments<Options>(args)
             .WithParsed<Options>(options => {
                 if (options.Ephemeral)
                 {
@@ -67,7 +67,7 @@ public class Program
                 }
             })
             .WithNotParsed(errors => 
-            {
+            { 
                 foreach(var error in errors)
                 {
                     Console.WriteLine(error);
@@ -100,5 +100,60 @@ public class Program
         Console.WriteLine($"Writing to file '{filePath}'");
         File.WriteAllText(filePath, content);
         Console.WriteLine($"Written to file '{filePath}'");
+    }
+
+    private static string[] AppendEnvironmentVariables(string[] args)
+    {
+        var options = GetOptions();
+
+        var newArgs = new List<string>(args);
+        foreach (var unusedOption in FilterOptions(args, options))
+        {
+            // repalced this 'Assembly.GetExecutingAssembly().GetName().Name.ToUpperInvariant()' with a static prefix of 'SC_'
+            // ignore verbs since this solution has only 1
+            var value = Environment.GetEnvironmentVariable($"SC_{unusedOption.LongName.ToUpperInvariant()}");
+            if (value != null)
+            {
+                newArgs.Add($"--{unusedOption.LongName}={value}");
+            }
+        }
+        return newArgs.ToArray();
+    }
+
+    private static OptionAttribute[] GetOptions()
+    {
+        return typeof(Options).GetProperties()
+            .Select(property => property.GetCustomAttributes<OptionAttribute>().FirstOrDefault())
+            .Where(option => option != null).ToArray();
+    }
+
+    private static OptionAttribute[] FilterOptions(string[] args, OptionAttribute[] options)
+    {
+        var usedLongNames = new HashSet<string>();
+        var usedShortNames = new HashSet<string>();
+
+        foreach (var arg in args)
+        {
+            if (arg.StartsWith("--"))
+            {
+                var longName = arg.Substring(2);
+                if (longName.Contains('='))
+                {
+                    longName = longName.Substring(0, longName.IndexOf('='));
+                }
+                usedLongNames.Add(longName);
+            }
+            else if (arg.StartsWith("-"))
+            {
+                var shortName = arg.Substring(1);
+                if (shortName.Contains('='))
+                {
+                    shortName = shortName.Substring(0, shortName.IndexOf('='));
+                }
+                usedShortNames.Add(shortName);
+            }
+        }
+
+        return options.Where(option => !usedLongNames.Contains(option.LongName) && !usedShortNames.Contains(option.ShortName)).ToArray();
     }
 }
